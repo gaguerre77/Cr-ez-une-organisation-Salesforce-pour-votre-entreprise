47c6207b2b5e3a929ef184798001bef7b6c44c9a
Georges <georges.aguerre@wanadoo.fr>
2025-01-07 17:17:25 +0100

    affichage de la version avec un tableau simple

--- OCPROJET4/force-app/main/default/lwc/opportunityProductsViewer/opportunityProductsViewer.js
+++ OCPROJET4/force-app/main/default/lwc/opportunityProductsViewer/opportunityProductsViewer.js
@@ -1,3 +1,32 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire, track } from 'lwc';
+import getOpportunityLineItems from '@salesforce/apex/OpportunityLineItemController.getOpportunityLineItems';
 
-export default class OpportunityProductsViewer extends LightningElement {}
+
+
+
+
+export default class OpportunityProductsViewer extends LightningElement {
+    @track opportunityLineItems;
+
+    columns = [
+        { label: 'Id', fieldName: 'Id' },
+        { label: 'Name', fieldName: 'Name' },
+        { label: 'Product2Id', fieldName: 'Product2Id' },
+        { label: 'Quantity', fieldName: 'Quantity', type: 'number' },
+        { label: 'Unit Price', fieldName: 'UnitPrice', type: 'currency' },
+        { label: 'List Price', fieldName: 'ListPrice', type: 'currency' },
+        { label: 'Total Price', fieldName: 'TotalPrice', type: 'currency' },
+        { label: 'Product Name', fieldName: 'Product2.Name' },
+        { label: 'Quantity In Stock', fieldName: 'Product2.QuantityInStock__c', type: 'number' }
+    ];
+
+
+    @wire(getOpportunityLineItems)
+    wiredOpportunityLineItems({ error, data }) {
+        if (data) {
+            this.opportunityLineItems = data;
+        } else if (error) {
+            console.error('Error retrieving opportunity line items:', error);
+        }
+    }
+}
